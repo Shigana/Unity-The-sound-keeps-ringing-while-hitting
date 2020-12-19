3efdf93b64086b116663740429924a9128fe2f29
Shigana <76201160+Shigana@users.noreply.github.com>
2020-12-19 20:14:40 +0900

    Add files via upload

---
+++ Audio.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+
+
+    // Use this for initialization
+    void Start()
+    { }
+
+    // Update is called once per frame
+    void Update()
+    { }
+    public AudioClip sound01;
+
+
+    void OnTriggerStay(Collider other) //OnTriggerStay continues to be called when an object is slipping through.
+    {
+        if (other.gameObject.CompareTag("WallF"))
+        {
+            GetComponent<AudioSource>().PlayOneShot(sound01);
+        }
+        if (other.gameObject.CompareTag("WallB"))
+        {
+            GetComponent<AudioSource>().PlayOneShot(sound01);
+        }
+        if (other.gameObject.CompareTag("WallR"))
+        {
+            GetComponent<AudioSource>().PlayOneShot(sound01);
+        }
+        if (other.gameObject.CompareTag("WallL"))
+        {
+            GetComponent<AudioSource>().PlayOneShot(sound01);
+        }
+    }
+}
